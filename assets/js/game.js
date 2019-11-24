010d70671027ee5ac4da6b84cf7bf2ac2adc2c88
Jim Gray <jimgray2005@gmail.com>
2019-11-24 09:46:47 -0500

    added css, added game logic functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -27,4 +27,30 @@ $("a").on("click", function () {
         total.text("Total: " + value);
       }
     });
+
+function drawCards(num) { 
+  // call to API for cards to draw
+}
+
+function checkScore() { 
+  const scoreTable = {
+    "2" : 2,
+    "3" : 3,
+    "4" : 4,
+    "5" : 5,
+    "6" : 6,
+    "7" : 7,
+    "8" : 8,
+    "9" : 9,
+    "10" : 10,
+    "JACK" : 10,
+    "QUEEN" : 10,
+    "KING" : 10,
+  }
+}
+
+function checkAces() {
+  // if treated as 1 or 11
+}
+
 });
