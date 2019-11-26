db09b8cfbe4a5caebe41409295624bb38237d361
Jim Gray <jimgray2005@gmail.com>
2019-11-25 22:52:56 -0500

    added card scoring logic, flipped first card for dealer

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,14 +1,23 @@
 // Event listener for all button elements
-var drawnCardsDiv = $("#drawnCards");
-var total = $('#total');
-var value = 0;
+let playerCardsDiv = $("#playerCards");
+let dealerCardsDiv = $("#dealerCards");
+let drawBtn = $("#drawBtn");
+let total = $('#total');
+let value = 0;
+let dealerHand = [];
+let playerHand = [];
 
-$("a").on("click", function () {
+$("#drawBtn").on("click", function () {
+  drawCards(drawBtn.attr("data-cards"), "new");
+  drawBtn.attr("data-cards", 1);
+});
 
-  var count = "2";
-  var deck = "new";
+function drawCards(num, game) { 
+  // call to API for cards to draw
+  let count = num;
+  let deck = game;
 
-  var queryURL = `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=${count}`
+  let queryURL = `https://deckofcardsapi.com/api/deck/${deck}/draw/?count=${count}`
 
   // Performing our AJAX GET request
   $.ajax({
@@ -21,31 +30,40 @@ $("a").on("click", function () {
       console.log(response)
       for (i = 0; i < response.cards.length; i++) {
         var cardImg = $("<img>");
-        cardImg.attr("src", response.cards[i].image);
-        drawnCardsDiv.append(cardImg);
-        value += parseInt(response.cards[i].value);
+        
+        dealerHand.push({
+          suit: response.cards[i].suit,
+          value: checkScore(response.cards[i].value),
+          code: response.cards[i].code,
+          image: response.cards[i].image,
+          hiddenImage: response.cards[i].image,
+          isFlipped: false,
+        });
+        
+        if(i == 0) { 
+          dealerHand[i].isFlipped = true;
+          cardImg.attr("src", "./assets/images/facedown_red.jpeg");
+        } else {
+          cardImg.attr("src", dealerHand[i].image);
+        }
+        cardImg.addClass("card");
+        dealerCardsDiv.append(cardImg);
+        value += parseInt(dealerHand[i].value);
         total.text("Total: " + value);
       }
     });
 
-function drawCards(num) { 
-  // call to API for cards to draw
+    console.log(dealerHand);
 }
 
-function checkScore() { 
-  const scoreTable = {
-    "2" : 2,
-    "3" : 3,
-    "4" : 4,
-    "5" : 5,
-    "6" : 6,
-    "7" : 7,
-    "8" : 8,
-    "9" : 9,
-    "10" : 10,
-    "JACK" : 10,
-    "QUEEN" : 10,
-    "KING" : 10,
+function checkScore(value) { 
+  if(value == "JACK" || value == "QUEEN" || value == "KING"){
+    return "10";
+  }
+  else if (value == "ACE") {
+    return checkAces(value);
+  } else {
+    return value;
   }
 }
 
@@ -53,4 +71,7 @@ function checkAces() {
   // if treated as 1 or 11
 }
 
-});
+function flipCards() {
+  // show the dealer's hidden card
+  dealerHand[0].isFlipped = false;
+}
